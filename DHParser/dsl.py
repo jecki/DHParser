9922348b210256932a529ed8ef96001619a7c3da
eckhart <eckhart.arnold@posteo.de>
2018-06-14 07:09:26 +0200

    - Korrekturen am EBNF: BelegText und BelegKern

--- DHParser/dsl.py
+++ DHParser/dsl.py
@@ -76,6 +76,7 @@ dhparserdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
 DHPARSER_IMPORTS = '''
+import collections
 from functools import partial
 import os
 import sys
@@ -92,14 +93,15 @@ from DHParser import logging, is_filename, load_if_file, \\
     Option, NegativeLookbehind, OneOrMore, RegExp, Retrieve, Series, RE, Capture, \\
     ZeroOrMore, Forward, NegativeLookahead, Required, mixin_comment, compile_source, \\
     grammar_changed, last_value, counterpart, accumulate, PreprocessorFunc, \\
-    Node, TransformationFunc, TransformationDict, \\
+    Node, TransformationFunc, TransformationDict, transformation_factory, \\
     traverse, remove_children_if, merge_children, is_anonymous, \\
     reduce_single_child, replace_by_single_child, replace_or_reduce, remove_whitespace, \\
     remove_expendables, remove_empty, remove_tokens, flatten, is_whitespace, \\
     is_empty, is_expendable, collapse, replace_content, WHITESPACE_PTYPE, TOKEN_PTYPE, \\
     remove_nodes, remove_content, remove_brackets, replace_parser, remove_anonymous_tokens, \\
     keep_children, is_one_of, has_content, apply_if, remove_first, remove_last, \\
-    remove_anonymous_empty, keep_nodes, traverse_locally, strip, lstrip, rstrip
+    remove_anonymous_empty, keep_nodes, traverse_locally, strip, lstrip, rstrip, \\
+    replace_content, replace_content_by
 '''.format(dhparserdir=dhparserdir)
 
 
