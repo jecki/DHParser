9760c37361419689ce82c4891532a19b63dc6a7d
eckhart <eckhart.arnold@posteo.de>
2018-08-20 21:30:09 +0200

    test/run.py ammended

--- test/run.py
+++ test/run.py
@@ -4,21 +4,42 @@
 
 if __name__ == "__main__":
     import os
+    import platform
+
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
 
     # if os.getcwd().endswith('test'):
     #     os.chdir('..')
     # print("Running nosetests:")
     # os.system("nosetests test")
+    if platform.system() != "Windows":
+        interpreters = ['python ', 'pypy3 ', 'python37 ']
+    else:
+        interpreters = [r'C:\Users\di68kap\AppData\Local\Programs\Python\Python37-32\python.exe ']
 
-    # interpreters = ['python ', 'pypy3 ', 'python37 ']
-    interpreters = [r'C:\Users\di68kap\AppData\Local\Programs\Python\Python37-32\python.exe ']
+    cwd = os.getcwd()
 
     for interpreter in interpreters:
         os.system(interpreter + '--version')
+
+        # unit tests
+
+        os.chdir(scriptdir)
 
         assert os.getcwd().endswith('test')
         files = os.listdir()
         for filename in files:
             if filename.startswith('test_'):
-                print('\nTEST ' + filename)
+                print('\nUNITTEST ' + filename)
                 os.system(interpreter + filename)
+
+        # doctests
+
+        os.chdir('..')
+        files = os.listdir('DHParser')
+        for filename in files:
+            if filename.endswith('.py'):
+                print('\nDOCTESTS in ' + filename)
+                os.system(interpreter + ' -m doctest ' + filename)
+
+    os.chdir(cwd)
