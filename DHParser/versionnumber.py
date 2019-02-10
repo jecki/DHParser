74c1a803a8095b9c7ea12e5de6e68cedb0bf86c0
eckhart <eckhart.arnold@posteo.de>
2019-02-10 15:08:17 +0100

    - README.md updated

--- DHParser/versionnumber.py
+++ DHParser/versionnumber.py
@@ -16,4 +16,4 @@
 # permissions and limitations under the License.
 
 __all__ = ('__version__',)
-__version__ = '0.8.4'  # + '_dev' + str(os.stat(__file__).st_mtime)
+__version__ = '0.8.5'  # + '_dev' + str(os.stat(__file__).st_mtime)
--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',  # untested!
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
