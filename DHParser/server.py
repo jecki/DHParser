04c42c5bb0954b1733308116a0799e96f22328d4
eckhart <eckhart.arnold@posteo.de>
2020-08-07 22:09:28 +0200

    server.py: StreamReaderProxy and StreamWriterProxy only allow ttys as file-like objects

--- DHParser/server.py
+++ DHParser/server.py
@@ -459,6 +459,7 @@ class StreamReaderProxy:
             self.buffered_io = io_reader.buffer
         except AttributeError:
             self.buffered_io = io_reader
+        assert self.buffered_io.isatty()
         self.loop = asyncio.get_running_loop() if sys.version_info >= (3, 7) \
             else asyncio.get_event_loop()
 
@@ -487,6 +488,7 @@ class StreamWriterProxy:
             self.buffered_io = io_writer.buffer
         except AttributeError:
             self.buffered_io = io_writer
+        assert self.buffered_io.isatty()
         self.buffer = []
         self.loop = asyncio.get_running_loop() if sys.version_info >= (3, 7) \
             else asyncio.get_event_loop()
