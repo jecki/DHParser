7a1311b5b8b424700b452a035cd8b28e909dc4c3
Eckhart Arnold <eckhart.arnold@posteo.de>
2021-02-02 12:27:40 +0100

    examples updated / fixed

---
+++ examples/Arithmetic/ArithmeticParser.py
@@ -0,0 +1,410 @@
+#!/usr/bin/env python3
+
+#######################################################################
+#
+# SYMBOLS SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+
+import collections
+from functools import partial
+import os
+import sys
+from typing import Tuple, List, Union, Any, Optional, Callable
+
+try:
+    scriptpath = os.path.dirname(__file__)
+except NameError:
+    scriptpath = ''
+dhparser_parentdir = os.path.abspath(os.path.join(scriptpath, r'../..'))
+if scriptpath not in sys.path:
+    sys.path.append(scriptpath)
+if dhparser_parentdir not in sys.path:
+    sys.path.append(dhparser_parentdir)
+
+try:
+    import regex as re
+except ImportError:
+    import re
+from DHParser import start_logging, suspend_logging, resume_logging, is_filename, load_if_file, \
+    Grammar, Compiler, nil_preprocessor, PreprocessorToken, Whitespace, Drop, AnyChar, \
+    Lookbehind, Lookahead, Alternative, Pop, Text, Synonym, Counted, Interleave, INFINITE, \
+    Option, NegativeLookbehind, OneOrMore, RegExp, Retrieve, Series, Capture, \
+    ZeroOrMore, Forward, NegativeLookahead, Required, mixin_comment, compile_source, \
+    grammar_changed, last_value, matching_bracket, PreprocessorFunc, is_empty, remove_if, \
+    Node, TransformationFunc, TransformationDict, transformation_factory, traverse, \
+    remove_children_if, move_adjacent, normalize_whitespace, is_anonymous, matches_re, \
+    reduce_single_child, replace_by_single_child, replace_or_reduce, remove_whitespace, \
+    replace_by_children, remove_empty, remove_tokens, flatten, all_of, any_of, \
+    merge_adjacent, collapse, collapse_children_if, transform_content, WHITESPACE_PTYPE, \
+    TOKEN_PTYPE, remove_children, remove_content, remove_brackets, change_tag_name, \
+    remove_anonymous_tokens, keep_children, is_one_of, not_one_of, has_content, apply_if, peek, \
+    remove_anonymous_empty, keep_nodes, traverse_locally, strip, lstrip, rstrip, \
+    transform_content, replace_content_with, forbid, assert_content, remove_infix_operator, \
+    add_error, error_on, recompile_grammar, left_associative, lean_left, set_config_value, \
+    get_config_value, node_maker, access_thread_locals, access_presets, \
+    finalize_presets, ErrorCode, RX_NEVER_MATCH, set_tracer, resume_notices_on, \
+    trace_history, has_descendant, neg, has_ancestor, optional_last_value, insert, \
+    positions_of, replace_tag_names, add_attributes, delimit_children, merge_connected, \
+    has_attr, has_parent, ThreadLocalSingletonFactory, Error, canonical_error_strings, \
+    has_errors, ERROR, FATAL, set_preset_value, get_preset_value
+
+
+#######################################################################
+#
+# PREPROCESSOR SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+def nop(arg):
+    return arg
+
+
+def ArithmeticPreprocessor(text):
+    return text, nop
+
+
+def get_preprocessor() -> PreprocessorFunc:
+    return ArithmeticPreprocessor
+
+
+#######################################################################
+#
+# PARSER SECTION - Don't edit! CHANGES WILL BE OVERWRITTEN!
+#
+#######################################################################
+
+class ArithmeticGrammar(Grammar):
+    r"""Parser for an Arithmetic source file.
+    """
+    expression = Forward()
+    source_hash__ = "63317dae9799e961704579764f281fcd"
+    anonymous__ = re.compile('..(?<=^)')
+    static_analysis_pending__ = []  # type: List[bool]
+    parser_initialization__ = ["upon instantiation"]
+    COMMENT__ = r'#.*'
+    comment_rx__ = re.compile(COMMENT__)
+    WHITESPACE__ = r'\s*'
+    WSP_RE__ = mixin_comment(whitespace=WHITESPACE__, comment=COMMENT__)
+    wsp__ = Whitespace(WSP_RE__)
+    dwsp__ = Drop(Whitespace(WSP_RE__))
+    VARIABLE = Series(RegExp('[A-Za-z]'), dwsp__)
+    NUMBER = Series(RegExp('(?:0|(?:[1-9]\\d*))(?:\\.\\d+)?'), dwsp__)
+    NEGATIVE = RegExp('[-]')
+    POSITIVE = RegExp('[+]')
+    DIV = Series(Text("/"), dwsp__)
+    MUL = Series(Text("*"), dwsp__)
+    MINUS = Series(Text("-"), dwsp__)
+    PLUS = Series(Text("+"), dwsp__)
+    group = Series(Series(Drop(Text("(")), dwsp__), expression, Series(Drop(Text(")")), dwsp__))
+    sign = Alternative(POSITIVE, NEGATIVE)
+    factor = Series(Option(sign), Alternative(NUMBER, VARIABLE, group), ZeroOrMore(Alternative(VARIABLE, group)))
+    term = Series(factor, ZeroOrMore(Series(Alternative(DIV, MUL), factor)))
+    expression.set(Series(term, ZeroOrMore(Series(Alternative(PLUS, MINUS), term))))
+    root__ = expression
+    
+
+_raw_grammar = ThreadLocalSingletonFactory(ArithmeticGrammar, ident=1)
+
+def get_grammar() -> ArithmeticGrammar:
+    grammar = _raw_grammar()
+    if get_config_value('resume_notices'):
+        resume_notices_on(grammar)
+    elif get_config_value('history_tracking'):
+        set_tracer(grammar, trace_history)
+    return grammar
+    
+def parse_Arithmetic(document, start_parser = "root_parser__", *, complete_match=True):
+    return get_grammar()(document, start_parser, complete_match)
+
+
+#######################################################################
+#
+# AST SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+Arithmetic_AST_transformation_table = {
+    # AST Transformations for the Arithmetic-grammar
+    "<": flatten,
+    "expression": [],
+    "term": [],
+    "factor": [],
+    "sign": [],
+    "group": [],
+    "PLUS": [],
+    "MINUS": [],
+    "MUL": [],
+    "DIV": [],
+    "POSITIVE": [],
+    "NEGATIVE": [],
+    "NUMBER": [],
+    "VARIABLE": [],
+    "*": replace_by_single_child
+}
+
+
+def ArithmeticTransformer() -> TransformationFunc:
+    """Creates a transformation function that does not share state with other
+    threads or processes."""
+    return partial(traverse, processing_table=Arithmetic_AST_transformation_table.copy())
+
+get_transformer = ThreadLocalSingletonFactory(ArithmeticTransformer, ident=1)
+
+def transform_Arithmetic(cst):
+    get_transformer()(cst)
+
+
+#######################################################################
+#
+# COMPILER SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+class ArithmeticCompiler(Compiler):
+    """Compiler for the abstract-syntax-tree of a Arithmetic source file.
+    """
+
+    def __init__(self):
+        super(ArithmeticCompiler, self).__init__()
+
+    def reset(self):
+        super().reset()
+        # initialize your variables here, not in the constructor!
+
+    def on_expression(self, node):
+        return self.fallback_compiler(node)
+
+    # def on_term(self, node):
+    #     return node
+
+    # def on_factor(self, node):
+    #     return node
+
+    # def on_sign(self, node):
+    #     return node
+
+    # def on_group(self, node):
+    #     return node
+
+    # def on_PLUS(self, node):
+    #     return node
+
+    # def on_MINUS(self, node):
+    #     return node
+
+    # def on_MUL(self, node):
+    #     return node
+
+    # def on_DIV(self, node):
+    #     return node
+
+    # def on_POSITIVE(self, node):
+    #     return node
+
+    # def on_NEGATIVE(self, node):
+    #     return node
+
+    # def on_NUMBER(self, node):
+    #     return node
+
+    # def on_VARIABLE(self, node):
+    #     return node
+
+
+get_compiler = ThreadLocalSingletonFactory(ArithmeticCompiler, ident=1)
+
+def compile_Arithmetic(ast):
+    return get_compiler()(ast)
+
+
+#######################################################################
+#
+# END OF DHPARSER-SECTIONS
+#
+#######################################################################
+
+RESULT_FILE_EXTENSION = ".sxpr"  # Change this according to your needs!
+
+
+def compile_src(source: str) -> Tuple[Any, List[Error]]:
+    """Compiles ``source`` and returns (result, errors, ast)."""
+    result_tuple = compile_source(source, get_preprocessor(), get_grammar(), get_transformer(),
+                                  get_compiler())
+    return result_tuple[:2]  # drop the AST at the end of the result tuple
+
+
+def serialize_result(result: Any) -> Union[str, bytes]:
+    """Serialization of result. REWRITE THIS, IF YOUR COMPILATION RESULT
+    IS NOT A TREE OF NODES.
+    """
+    if isinstance(result, Node):
+        return result.serialize(how='default' if RESULT_FILE_EXTENSION != '.xml' else 'xml')
+    else:
+        return repr(result)
+
+
+def process_file(source: str, result_filename: str = '') -> str:
+    """Compiles the source and writes the serialized results back to disk,
+    unless any fatal errors have occurred. Error and Warning messages are
+    written to a file with the same name as `result_filename` with an
+    appended "_ERRORS.txt" or "_WARNINGS.txt" in place of the name's
+    extension. Returns the name of the error-messages file or an empty
+    string, if no errors of warnings occurred.
+    """
+    source_filename = source if is_filename(source) else ''
+    result, errors = compile_src(source)
+    if not has_errors(errors, FATAL):
+        if os.path.abspath(source_filename) != os.path.abspath(result_filename):
+            with open(result_filename, 'w') as f:
+                f.write(serialize_result(result))
+        else:
+            errors.append(Error('Source and destination have the same name "%s"!'
+                                % result_filename, 0, FATAL))
+    if errors:
+        err_ext = '_ERRORS.txt' if has_errors(errors, ERROR) else '_WARNINGS.txt'
+        err_filename = os.path.splitext(result_filename)[0] + err_ext
+        with open(err_filename, 'w') as f:
+            f.write('\n'.join(canonical_error_strings(errors, source_filename)))
+        return err_filename
+    return ''
+
+
+def batch_process(file_names: List[str], out_dir: str,
+                  *, submit_func: Callable = None,
+                  log_func: Callable = None) -> List[str]:
+    """Compiles all files listed in filenames and writes the results and/or
+    error messages to the directory `our_dir`. Returns a list of error
+    messages files.
+    """
+    error_list =  []
+
+    def gen_dest_name(name):
+        return os.path.join(out_dir, os.path.splitext(os.path.basename(name))[0] \
+                                     + RESULT_FILE_EXTENSION)
+
+    def run_batch(submit_func: Callable):
+        nonlocal error_list
+        err_futures = []
+        for name in file_names:
+            dest_name = gen_dest_name(name)
+            err_futures.append(submit_func(process_file, name, dest_name))
+        for file_name, err_future in zip(file_names, err_futures):
+            error_filename = err_future.result()
+            if log_func:
+                log_func('Compiling "%s"' % file_name)
+            if error_filename:
+                error_list.append(error_filename)
+
+    if submit_func is None:
+        import concurrent.futures
+        from DHParser.toolkit import instantiate_executor
+        with instantiate_executor(get_config_value('batch_processing_parallelization'),
+                                  concurrent.futures.ProcessPoolExecutor) as pool:
+            run_batch(pool.submit)
+    else:
+        run_batch(submit_func)
+    return error_list
+
+
+if __name__ == "__main__":
+    # recompile grammar if needed
+    if __file__.endswith('Parser.py'):
+        grammar_path = os.path.abspath(__file__).replace('Parser.py', '.ebnf')
+    else:
+        grammar_path = os.path.splitext(__file__)[0] + '.ebnf'
+    parser_update = False
+
+    def notify():
+        global parser_update
+        parser_update = True
+        print('recompiling ' + grammar_path)
+
+    if os.path.exists(grammar_path) and os.path.isfile(grammar_path):
+        if not recompile_grammar(grammar_path, force=False, notify=notify):
+            error_file = os.path.basename(__file__).replace('Parser.py', '_ebnf_ERRORS.txt')
+            with open(error_file, encoding="utf-8") as f:
+                print(f.read())
+            sys.exit(1)
+        elif parser_update:
+            print(os.path.basename(__file__) + ' has changed. '
+                  'Please run again in order to apply updated compiler')
+            sys.exit(0)
+    else:
+        print('Could not check whether grammar requires recompiling, '
+              'because grammar was not found at: ' + grammar_path)
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="Parses a Arithmetic-file and shows its syntax-tree.")
+    parser.add_argument('files', nargs='+')
+    parser.add_argument('-d', '--debug', action='store_const', const='debug',
+                        help='Store debug information in LOGS subdirectory')
+    parser.add_argument('-x', '--xml', action='store_const', const='xml',
+                        help='Store result as XML instead of S-expression')
+    parser.add_argument('-o', '--out', nargs=1, default=['out'],
+                        help='Output directory for batch processing')
+    parser.add_argument('-v', '--verbose', action='store_const', const='verbose',
+                        help='Verbose output')
+    parser.add_argument('--singlethread', action='store_const', const='singlethread',
+                        help='Sun batch jobs in a single thread (recommended only for debugging)')
+
+    args = parser.parse_args()
+    file_names, out, log_dir = args.files, args.out[0], ''
+
+    # if not os.path.exists(file_name):
+    #     print('File "%s" not found!' % file_name)
+    #     sys.exit(1)
+    # if not os.path.isfile(file_name):
+    #     print('"%s" is not a file!' % file_name)
+    #     sys.exit(1)
+
+    if args.debug is not None:
+        log_dir = 'LOGS'
+        set_config_value('history_tracking', True)
+        set_config_value('resume_notices', True)
+        set_config_value('log_syntax_trees', set(['cst', 'ast']))  # don't use a set literal, here
+    start_logging(log_dir)
+
+    if args.xml:
+        RESULT_FILE_EXTENSION = '.xml'
+
+    def echo(message: str):
+        if args.verbose:
+            print(message)
+
+    batch_processing = True
+    if len(file_names) == 1:
+        if os.path.isdir(file_names[0]):
+            dir_name = file_names[0]
+            echo('Processing all files in directory: ' + dir_name)
+            file_names = [os.path.join(dir_name, fn) for fn in os.listdir(dir_name)
+                          if os.path.isfile(os.path.join(dir_name, fn))]
+        elif not ('-o' in sys.argv or '--out' in sys.argv):
+            batch_processing = False
+
+    if batch_processing:
+        if not os.path.exists(out):
+            os.mkdir(out)
+        elif not os.path.isdir(out):
+            print('Output directory "%s" exists and is not a directory!' % out)
+            sys.exit(1)
+        error_files = batch_process(file_names, out, log_func=print if args.verbose else None)
+        if error_files:
+            category = "ERRORS" if any(f.endswith('_ERRORS.txt') for f in error_files) \
+                else "warnings"
+            print("There have been %s! Please check files:" % category)
+            print('\n'.join(error_files))
+            if category == "ERRORS":
+                sys.exit(1)
+    else:
+        result, errors = compile_src(file_names[0])
+
+        if errors:
+            for err_str in canonical_error_strings(errors, file_names[0]):
+                print(err_str)
+            if has_errors(errors, ERROR):
+                sys.exit(1)
+
+        print(result.serialize(how='default' if args.xml is None else 'xml')
+              if isinstance(result, Node) else result)
---
+++ examples/Arithmetic/LameArithmeticParser.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+
+#######################################################################
+#
+# SYMBOLS SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+
+import collections
+from functools import partial
+import os
+import sys
+
+try:
+    scriptpath = os.path.dirname(__file__)
+except NameError:
+    scriptpath = ''
+dhparser_parentdir = os.path.abspath(os.path.join(scriptpath, r'../..'))
+if scriptpath not in sys.path:
+    sys.path.append(scriptpath)
+if dhparser_parentdir not in sys.path:
+    sys.path.append(dhparser_parentdir)
+
+try:
+    import regex as re
+except ImportError:
+    import re
+from DHParser import start_logging, suspend_logging, resume_logging, is_filename, load_if_file, \
+    Grammar, Compiler, nil_preprocessor, PreprocessorToken, Whitespace, Drop, AnyChar, \
+    Lookbehind, Lookahead, Alternative, Pop, Text, Synonym, Counted, Interleave, INFINITE, \
+    Option, NegativeLookbehind, OneOrMore, RegExp, Retrieve, Series, Capture, \
+    ZeroOrMore, Forward, NegativeLookahead, Required, mixin_comment, compile_source, \
+    grammar_changed, last_value, matching_bracket, PreprocessorFunc, is_empty, remove_if, \
+    Node, TransformationFunc, TransformationDict, transformation_factory, traverse, \
+    remove_children_if, move_adjacent, normalize_whitespace, is_anonymous, matches_re, \
+    reduce_single_child, replace_by_single_child, replace_or_reduce, remove_whitespace, \
+    replace_by_children, remove_empty, remove_tokens, flatten, all_of, any_of, \
+    merge_adjacent, collapse, collapse_children_if, transform_content, WHITESPACE_PTYPE, \
+    TOKEN_PTYPE, remove_children, remove_content, remove_brackets, change_tag_name, \
+    remove_anonymous_tokens, keep_children, is_one_of, not_one_of, has_content, apply_if, peek, \
+    remove_anonymous_empty, keep_nodes, traverse_locally, strip, lstrip, rstrip, \
+    transform_content, replace_content_with, forbid, assert_content, remove_infix_operator, \
+    add_error, error_on, recompile_grammar, left_associative, lean_left, set_config_value, \
+    get_config_value, XML_SERIALIZATION, SXPRESSION_SERIALIZATION, node_maker, \
+    INDENTED_SERIALIZATION, JSON_SERIALIZATION, access_thread_locals, access_presets, \
+    finalize_presets, ErrorCode, RX_NEVER_MATCH, set_tracer, resume_notices_on, \
+    trace_history, has_descendant, neg, has_ancestor, optional_last_value, insert, \
+    positions_of, replace_tag_names, add_attributes, delimit_children, merge_connected, \
+    has_attr, has_parent
+
+
+#######################################################################
+#
+# PREPROCESSOR SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+def LameArithmeticPreprocessor(text):
+    return text, lambda i: i
+
+
+def get_preprocessor() -> PreprocessorFunc:
+    return LameArithmeticPreprocessor
+
+
+#######################################################################
+#
+# PARSER SECTION - Don't edit! CHANGES WILL BE OVERWRITTEN!
+#
+#######################################################################
+
+class LameArithmeticGrammar(Grammar):
+    r"""Parser for a LameArithmetic source file.
+    """
+    expr = Forward()
+    term = Forward()
+    source_hash__ = "d68d04adb34c7294d6508a62e1c269d9"
+    anonymous__ = re.compile('..(?<=^)')
+    static_analysis_pending__ = []  # type: List[bool]
+    parser_initialization__ = ["upon instantiation"]
+    COMMENT__ = r''
+    comment_rx__ = RX_NEVER_MATCH
+    WHITESPACE__ = r'\s*'
+    WSP_RE__ = mixin_comment(whitespace=WHITESPACE__, comment=COMMENT__)
+    wsp__ = Whitespace(WSP_RE__)
+    factor = Series(RegExp('[0-9]+'), wsp__)
+    term.set(Alternative(Series(term, Alternative(Series(Text("*"), wsp__), Series(Text("/"), wsp__)), factor), factor))
+    expr.set(Alternative(Series(expr, Alternative(Series(Text("+"), wsp__), Series(Text("-"), wsp__)), term), term))
+    formula = Series(Option(Series(RegExp(''), wsp__)), expr)
+    root__ = formula
+    
+
+_raw_grammar = ThreadLocalSingletonFactory(LameArithmeticGrammar, ident=1)
+
+def get_grammar() -> LameArithmeticGrammar:
+    grammar = _raw_grammar()
+    if get_config_value('resume_notices'):
+        resume_notices_on(grammar)
+    elif get_config_value('history_tracking'):
+        set_tracer(grammar, trace_history)
+    return grammar
+    
+def parse_LameArithmetic(document, start_parser = "root_parser__", *, complete_match=True):
+    return get_grammar()(document, start_parser, complete_match)
+
+
+#######################################################################
+#
+# AST SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+LameArithmetic_AST_transformation_table = {
+    # AST Transformations for the LameArithmetic-grammar
+    "<": flatten,
+    "formula": [],
+    "expr": [],
+    "term": [],
+    "factor": [],
+    "*": replace_by_single_child
+}
+
+
+
+def CreateLameArithmeticTransformer() -> TransformationFunc:
+    """Creates a transformation function that does not share state with other
+    threads or processes."""
+    return partial(traverse, processing_table=LameArithmetic_AST_transformation_table.copy())
+
+
+def get_transformer() -> TransformationFunc:
+    """Returns a thread/process-exclusive transformation function."""
+    THREAD_LOCALS = access_thread_locals()
+    try:
+        transformer = THREAD_LOCALS.LameArithmetic_00000001_transformer_singleton
+    except AttributeError:
+        THREAD_LOCALS.LameArithmetic_00000001_transformer_singleton = CreateLameArithmeticTransformer()
+        transformer = THREAD_LOCALS.LameArithmetic_00000001_transformer_singleton
+    return transformer
+
+
+#######################################################################
+#
+# COMPILER SECTION - Can be edited. Changes will be preserved.
+#
+#######################################################################
+
+class LameArithmeticCompiler(Compiler):
+    """Compiler for the abstract-syntax-tree of a LameArithmetic source file.
+    """
+
+    def __init__(self):
+        super(LameArithmeticCompiler, self).__init__()
+
+    def reset(self):
+        super().reset()
+        # initialize your variables here, not in the constructor!
+
+    def on_formula(self, node):
+        return self.fallback_compiler(node)
+
+    # def on_expr(self, node):
+    #     return node
+
+    # def on_term(self, node):
+    #     return node
+
+    # def on_factor(self, node):
+    #     return node
+
+
+
+def get_compiler() -> LameArithmeticCompiler:
+    """Returns a thread/process-exclusive LameArithmeticCompiler-singleton."""
+    THREAD_LOCALS = access_thread_locals()
+    try:
+        compiler = THREAD_LOCALS.LameArithmetic_00000001_compiler_singleton
+    except AttributeError:
+        THREAD_LOCALS.LameArithmetic_00000001_compiler_singleton = LameArithmeticCompiler()
+        compiler = THREAD_LOCALS.LameArithmetic_00000001_compiler_singleton
+    return compiler
+
+
+#######################################################################
+#
+# END OF DHPARSER-SECTIONS
+#
+#######################################################################
+
+def compile_src(source: str):
+    """Compiles ``source`` and returns (result, errors, ast).
+    """
+    result_tuple = compile_source(source, get_preprocessor(), get_grammar(), get_transformer(),
+                                  get_compiler())
+    return result_tuple
+
+
+if __name__ == "__main__":
+    # recompile grammar if needed
+    if __file__.endswith('Parser.py'):
+        grammar_path = os.path.abspath(__file__).replace('Parser.py', '.ebnf')
+    else:
+        grammar_path = os.path.splitext(__file__)[0] + '.ebnf'
+    parser_update = False
+
+    def notify():
+        global parser_update
+        parser_update = True
+        print('recompiling ' + grammar_path)
+
+    if os.path.exists(grammar_path) and os.path.isfile(grammar_path):
+        if not recompile_grammar(grammar_path, force=False, notify=notify):
+            error_file = os.path.basename(__file__).replace('Parser.py', '_ebnf_ERRORS.txt')
+            with open(error_file, encoding="utf-8") as f:
+                print(f.read())
+            sys.exit(1)
+        elif parser_update:
+            print(os.path.basename(__file__) + ' has changed. '
+                  'Please run again in order to apply updated compiler')
+            sys.exit(0)
+    else:
+        print('Could not check whether grammar requires recompiling, '
+              'because grammar was not found at: ' + grammar_path)
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="Parses a LameArithmetic-file and shows its syntax-tree.")
+    parser.add_argument('files', nargs=1)
+    parser.add_argument('-d', '--debug', action='store_const', const='debug')
+    parser.add_argument('-x', '--xml', action='store_const', const='xml')
+
+    args = parser.parse_args()
+    file_name, log_dir = args.files[0], ''
+
+    if not os.path.exists(file_name):
+        print('File "%s" not found!' % file_name)
+        sys.exit(1)
+    if not os.path.isfile(file_name):
+        print('"%s" is not a file!' % file_name)
+        sys.exit(1)
+
+    if args.debug is not None:
+        log_dir = 'LOGS'
+        set_config_value('history_tracking', True)
+        set_config_value('resume_notices', True)
+        set_config_value('log_syntax_trees', set(['cst', 'ast']))  # don't use a set literal, here
+    start_logging(log_dir)
+
+    result, errors, _ = compile_src(file_name)
+
+    if errors:
+        cwd = os.getcwd()
+        rel_path = file_name[len(cwd):] if file_name.startswith(cwd) else file_name
+        for error in errors:
+            print(rel_path + ':' + str(error))
+        sys.exit(1)
+    else:
+        print(result.serialize(how='default' if args.xml is None else 'xml')
+              if isinstance(result, Node) else result)
