3daf2baeebc44a94695c2c9a0764a24ea6a90c84
di68kap <EweKiT/gtbasG>
2022-04-08 08:28:28 +0200

    op cit autoren und Werke

--- DHParser/parse.py
+++ DHParser/parse.py
@@ -758,16 +758,17 @@ class Parser:
         calls is the method `apply()` without underscore!
         """
         if not flip(func, self.cycle_detection):
-            context.append(self)
+            # context.append(self)
+            context = context + [self]
             if func(context):
-                context.pop()
+                # context.pop()
                 return True
             else:
                 for parser in self.sub_parsers():
                     if parser._apply(func, context, flip):
-                        context.pop()
+                        # context.pop()
                         return True
-                context.pop()
+                # context.pop()
                 return False
         return False
 
