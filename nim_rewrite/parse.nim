3c56a9a4d66a59bf512aa190c186d64a7bbc9ab0
Eckhart Arnold <eckhart.arnold@posteo.de>
2022-12-08 18:13:37 +0100

    nim code extended

--- nim_rewrite/parse.nim
+++ nim_rewrite/parse.nim
@@ -1,8 +1,9 @@
+import std/options
 import nodetree
 
 
 type
-  ParsingResult = tuple[node: Node, location: int32]
+  ParsingResult = tuple[node: Option[Node], location: int32]
   ParseProc = proc(parser: Parser, location: int32): ParsingResult
   Parser = ref object of RootObj
     name: string
@@ -22,6 +23,21 @@ type
       text: string
 
 
+proc parsePlaceHolder(parser:Parser, location: int32): ParsingResult =
+  result = (none(Node), int32(0))
+
+
+proc initParser(parser: Parser) =
+  parser.name = ""
+  parser.parserType = "Parser"
+  parser.disposable = false
+  parser.dropContent = false
+  parser.grammar = GrammarPlaceholderSingleton
+  parser.symbol = 
+
+
+# func `()`(parser: Parser, location: int32): ParsingResult =
+
 
 
 ## Test-code
@@ -30,6 +46,5 @@ let
   p = Parser(name: "")
   t = Text(name: "", text: "abc")
 
-echo t is Parser
-
+echo r.node.get().name
 
