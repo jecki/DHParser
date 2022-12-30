ea2985171d18dcd8028497b710fe1fb50c4a0211
eckhart <eckhart.arnold@posteo.de>
2022-12-31 00:51:57 +0100

    additions to nim-rewrite

--- nim_rewrite/oop.nim
+++ nim_rewrite/oop.nim
@@ -3,8 +3,11 @@
 #   ## requires the init-procedure to return the object passed as first parameter
 #   new(T).`init`(args)
 
-template newObj*(T: typedesc, args: varargs[untyped]): untyped =
-  let obj = new(T)
-  obj.init(args)
-  obj
+# template newObj*(T: typedesc, args: varargs[untyped]): untyped =
+#   let obj = new(T)
+#   obj.init(args)
+#   obj
 
+template newObj*(T: typedesc, args: varargs[untyped]): T =
+  new(result)
+  result.init(args)
