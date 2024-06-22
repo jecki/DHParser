f37d35c9e9854f6d9b75cfe22579df4b29eb5344
Eckhart Arnold <eckhart.arnold@posteo.de>
2024-06-22 14:25:50 +0200

    wip

--- DHParser/configuration.py
+++ DHParser/configuration.py
@@ -648,7 +648,7 @@ ALLOWED_PRESET_VALUES['optimizations'] = frozenset({
     'alternative',
     'rearrange_alternative',  # this is also implied by 'alternative'
     'sequence'})
-CONFIG_PRESET['optimizations'] = frozenset({'alternative', 'sequence'})
+CONFIG_PRESET['optimizations'] = frozenset()
 
 
 ########################################################################
