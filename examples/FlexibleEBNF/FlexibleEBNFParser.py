7d11d75445aabcaf3f9f76b6141c2b56d88c005b
eckhart <arnold@badw.de>
2024-07-16 15:41:04 +0200

    lsp.py bugfixes

--- examples/FlexibleEBNF/FlexibleEBNFParser.py
+++ examples/FlexibleEBNF/FlexibleEBNFParser.py
@@ -108,7 +108,7 @@ class FlexibleEBNFGrammar(Grammar):
     element = Forward()
     expression = Forward()
     source_hash__ = "f28102ebed9fa2a532524a8a7fc322a7"
-    disposable__ = re.compile('(?:pure_elem$|countable$|MOD_SYM$|EOF$|FOLLOW_UP$|MOD_SEP$|ANY_SUFFIX$|component$|is_mdef$|no_range$)')
+    disposable__ = re.compile('(?:is_mdef$|pure_elem$|component$|FOLLOW_UP$|EOF$|no_range$|MOD_SYM$|MOD_SEP$|countable$|ANY_SUFFIX$)')
     static_analysis_pending__ = []  # type: List[bool]
     parser_initialization__ = ["upon instantiation"]
     error_messages__ = {'definition': [(re.compile(r','), 'Delimiter "," not expected in definition!\\nEither this was meant to be a directive and the directive symbol @ is missing\\nor the error is due to inconsistent use of the comma as a delimiter\\nfor the elements of a sequence.')]}
--- examples/XML_old/XMLParser.py
+++ examples/XML_old/XMLParser.py
@@ -80,7 +80,7 @@ class XMLGrammar(Grammar):
     element = Forward()
     source_hash__ = "c78a8dd65a4438562ada48b2cc53cd5a"
     early_tree_reduction__ = CombinedParser.MERGE_TREETOPS
-    disposable__ = re.compile('(?:PubidCharsSingleQuoted$|EOF$|Reference$|NameStartChar$|CData$|CommentChars$|PubidChars$|NameChars$|Misc$|EncName$|VersionNum$)')
+    disposable__ = re.compile('(?:NameChars$|CData$|NameStartChar$|EOF$|CommentChars$|Misc$|Reference$|VersionNum$|PubidChars$|EncName$|PubidCharsSingleQuoted$)')
     static_analysis_pending__ = []  # type: List[bool]
     parser_initialization__ = ["upon instantiation"]
     COMMENT__ = r''
