464273a5b99ac3e5bc1d91cc7aff11ff0a1a115a
eckhart <arnold@badw.de>
2025-07-22 19:05:01 +0200

    wip

---
+++ scratch/enivon.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+path = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+print(path)
--- scratch/interpreterpool.py
+++ scratch/interpreterpool.py
@@ -1,4 +1,6 @@
-import sys
+import sys, os
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
 
 class SomeClass:
     def __init__(self, val):
@@ -18,15 +20,27 @@ class TestInterpreterPoolWrapper:
     def test_interpreterpool(self):
         if sys.version_info >= (3, 14, 0):
             from concurrent.futures import InterpreterPoolExecutor
+            # save_pp = os.getenv("PYTHONPATH")
+            # if save_pp:
+            #     os.environ["PYTHONPATH"] = save_pp + ":" + script_dir
+            # else:
+            #     os.environ["PYTHONPATH"] = script_dir
+            import interpreterpool
             with InterpreterPoolExecutor() as ex:
-                result = ex.submit(task, 20)
+                result = ex.submit(interpreterpool.task, 20)
+            # if save_pp:
+            #     os.environ["PYTHONPATH"] = save_pp
+            # else:
+            #     os.unsetenv("PYTHONPATH")
             obj = result.result()
             assert obj.val == 20
+            print("YO!")
 
     def test_wrapped_interpreterpool(self):
         pass
 
 
 if __name__ == "__main__":
+    sys.path.append(os.path.abspath('..'))
     from DHParser.testing import runner
     runner("", globals())
