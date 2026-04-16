83d938865a40dd31286260c83cd9e28307e265d1
eckhart <eckhart.arnold@posteo.de>
2026-04-16 17:02:04 +0200

    parse.py: fix reversed history bug

--- DHParser/parse.py
+++ DHParser/parse.py
@@ -5367,13 +5367,13 @@ class Forward(UnaryParser):
             grammar.suspend_memoization__ = False
             rb_stack_size = len(grammar.rollback__)
             if history_tracking:
-                last_history_state = last_history_state + grammar.history__[history_pointer:len(grammar.history__)]
+                last_history_state.extend(grammar.history__[history_pointer:])
                 grammar.history__ = grammar.history__[:history_pointer]
             visited[location] = result
             next_result = self.parser(location)
 
         if history_tracking and result[0] is not None:
-            grammar.history__ = last_history_state + grammar.history__[:history_pointer]
+            grammar.history__ = grammar.history__[:history_pointer] + last_history_state
 
         self.recursion_counter[location] = False
         # Since the result of the last parser call (``next_result``) is discarded,
