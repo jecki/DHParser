f16935cabad4ffaa3b4cf37378428582cce4bbac
eckhart <eckhart.arnold@posteo.de>
2026-07-02 17:22:56 +0200

    Ollis MWG-Beispiele ContentMapping.markup() hinzugefügt (unter scratch)

--- scratch/markup/markup_demo.py
+++ scratch/markup/markup_demo.py
@@ -8,6 +8,11 @@ from DHParser.nodetree import Node, ContentMapping, parse_xml
 
 EXAMPLES_FILE = "VerlinkungsBeispiele.mwg.xml"
 
+
+def clean_snippet(cell: Node) -> str:
+    return re.sub(r'\s+', ' ', cell.as_xml(inline_tags={'cell'}, empty_tags={'lb'}))
+
+
 def read_xml_file(filename: str) -> list[dict[str, str]]:
     with open(filename, 'r', encoding='utf-8') as f:
         xml = f.read()
@@ -15,13 +20,12 @@ def read_xml_file(filename: str) -> list[dict[str, str]]:
     examples = []
     for row in tree.select('row'):
         if len(row.children) == 2 and row[1].children and 'ref' in row[1]:
-            before = re.sub(r'\s', row[0].as_xml(inline_tags={'cell'}, empty_tags={'lb'}))
             ref = row[1].pick('ref')
             target = ref.attr['target']
             typ = ref.get_attr('type', '')
-            ref_text = ''.join(ref.content for ref in row[1])
-            examples.append({'before': row[0].as_xml(inline_tags={'cell'}, empty_tags={'lb'}),
-                             'after':row[1].as_xml(inline_tags={'cell'}, empty_tags={'lb'}),
+            ref_text = re.sub(r'\s+', ' ', ''.join(ref.content for ref in row[1].select_children('ref')))
+            examples.append({'before': clean_snippet(row[0]),
+                             'after': clean_snippet(row[1]),
                              'replacements' : [{'ref': ref_text, 'target': target, 'type': typ}]})
     return examples
 
@@ -42,11 +46,12 @@ def markup(before, replacements: list[dict[str, str]]) -> str:
         typ = replacement.get('type', '')
         cm = ContentMapping(tree, ignore=IGNORE, divisibility=DIVISIBILITY)
         a = cm.content.find(ref)
-        assert a >= 0, ref
+        assert a >= 0, f'{ref}\n{cm.content}'
         b = a + len(ref)
-        attrs = {'target': target}
+        attrs = dict()
         if typ: attrs['type'] = typ
-        cm.markup(a, b, ref, attrs)
+        attrs['target'] = target
+        cm.markup(a, b, 'ref', attrs)
     return tree.as_xml(inline_tags={'cell'}, empty_tags={'lb'})
 
 
@@ -56,12 +61,12 @@ def markup_all_examples(examples):
         print(i)
         i += 1
         after = markup(example['before'], example['replacements'])
-        assert after == example['after']
+        assert after == example['after'], f'\n{after}\n{example["after"]}'
 
 
 def main():
     examples = read_xml_file(EXAMPLES_FILE)
-    # markup_all_examples(examples)
+    markup_all_examples(examples)
     json.dump(examples, sys.stdout, ensure_ascii=False, indent=2)
     sys.stdout.write('\n')
 
