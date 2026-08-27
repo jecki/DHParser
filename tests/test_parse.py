9c636de7efebefc7932fbff4d991260500023972
eckhart <arnold@badw.de>
2026-08-27 13:04:31 +0200

    tes case fixed

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -327,17 +327,15 @@ class TestInfiLoopsAndRecursion:
             log_parsing_history(arithmetic, "test_LeftRecursion_indirect3")
 
     def test_indirect_left_recursion4(self):
-        """See Hutchinson Squirrel Parser / Expansion Semantics
-        The semantics of DHParsers LR-Algorithm differ from the Squirrel Parser!
-        """
+        """See Hutchinson Squirrel Parser / Expansion Semantics"""
         ebnf = r"""
         A = (B !'y') | 'x'
         B = A 'x' 
         """
         parser = grammar_provider(ebnf)()
-        syntax_tree = parser('xxyx', complete_match=False)
+        syntax_tree = parser('xxxy', complete_match=False)
         assert not syntax_tree.errors
-        assert str(syntax_tree) == 'x'
+        assert str(syntax_tree) == 'xx'
 
     def test_left_recursion_side_effects(self):
         syntax = r"""@literalws = right
